17f63bb8e1050507decb66fcad4191c102427f48
twozerohigh <ghkfkd8542@naver.com>
2020-01-21 10:47:54 +0900

    first commit

---
+++ untitled.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Frame(object):
+    def setupUi(self, Frame):
+        Frame.setObjectName("Frame")
+        Frame.resize(1231, 707)
+        self.pushButton = QtWidgets.QPushButton(Frame)
+        self.pushButton.setGeometry(QtCore.QRect(190, 210, 271, 131))
+        self.pushButton.setObjectName("pushButton")
+        self.textEdit = QtWidgets.QTextEdit(Frame)
+        self.textEdit.setGeometry(QtCore.QRect(300, 90, 411, 71))
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Frame)
+        QtCore.QMetaObject.connectSlotsByName(Frame)
+
+    def retranslateUi(self, Frame):
+        _translate = QtCore.QCoreApplication.translate
+        Frame.setWindowTitle(_translate("Frame", "Frame"))
+        self.pushButton.setText(_translate("Frame", "PushButton"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Frame = QtWidgets.QFrame()
+    ui = Ui_Frame()
+    ui.setupUi(Frame)
+    Frame.show()
+    sys.exit(app.exec_())
+
